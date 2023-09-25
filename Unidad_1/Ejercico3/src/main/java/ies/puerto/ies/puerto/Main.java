f5062864b12da62a393a77856b988023b5a25560
José Manuel <71851066+Joseman2000LR@users.noreply.github.com>
2023-09-25 10:54:58 +0100

    cambios en ejercicio 3,4,2

---
+++ Unidad_1/Ejercico3/src/main/java/ies/puerto/ies/puerto/Main.java
@@ -0,0 +1,14 @@
+package ies.puerto;
+
+/*Clase que realiza el ejercicio3*/
+public class Main {
+    public static void main(String[] args) {
+        int n = 0;
+        int suma = 0;
+
+        for (n = 0; n < 10; n++) {
+            suma = n + 1;
+            System.out.print(suma + "+");
+        }
+    }
+}
--- Unidad_1/Ejercico4/src/main/java/ies/puerto/Main.java
+++ Unidad_1/Ejercico4/src/main/java/ies/puerto/Main.java
@@ -1,7 +1,8 @@
 package ies.puerto;
 
+/*Clase que realiza el ejercicio4*/
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }
