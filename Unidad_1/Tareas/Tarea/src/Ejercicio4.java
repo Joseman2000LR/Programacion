8d479a8bdd494ccbd63858f3ba7310edb9d82f60
José Manuel <71851066+Joseman2000LR@users.noreply.github.com>
2023-09-26 10:59:00 +0100

    creacionde ejercicio4

--- Unidad_1/Tareas/Tarea/src/Ejercicio4.java
+++ Unidad_1/Tareas/Tarea/src/Ejercicio4.java
@@ -1,13 +1,26 @@
+import java.util.Scanner;
+
 /**
- * @author Nombre del autor
+ * @author Joseman2000LR
  */
 public class Ejercicio4 {
 
     /**
-     * Descripcion del algoritmo
+     * Algoritmo que permmita leer un valor cualquiera N y escriba si es par o impar
+     * 
      * @param args
      */
     public static void main(String[] args) {
-       
+        int valorN;
+        Scanner lectura = new Scanner(System.in);
+        System.out.println("Introduzca  valor que quieras saber si es par o impar");
+        valorN = lectura.nextInt();
+        System.out.println("Valor de la variable es  : " + valorN);
+        if (valorN / 2 == 0) {
+            System.out.println("El numero " + valorN + "es par");
+        } else {
+            System.out.println("El numero " + valorN + "es impar");
+        }
+
     }
 }
