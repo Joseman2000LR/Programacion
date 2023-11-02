c73161a65d61ccd3f6328e83a84140cebcffa055
joseman2000 <jose12linares@gmail.com>
2023-11-02 15:28:18 +0000

    cambios y creaciion

--- Unidad_5/Ejercicios/metodosyArray/src/main/java/ies/puerto/Ejercicio10.java
+++ Unidad_5/Ejercicios/metodosyArray/src/main/java/ies/puerto/Ejercicio10.java
@@ -1,24 +1,12 @@
 package ies.puerto;
 
-public class Ejercicio6 {
-
+public class Ejercicio10 {
     public static void main(String[] args) {
         mostarMastrizOriginal();
-        matrizTranspuesta();
     }
 
     /**
-     * metodo que cambia fila y la columna de una matrix
-     */
-    public static void matrizTranspuesta() {
-        String resultado = "";
-        int[][] matrizOriginal = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
-        for (int i = 0; i < matrizOriginal.length; i++) {
-            System.out.println(matrizOriginal[0][i] + "" + matrizOriginal[1][i] + "" + matrizOriginal[2][i]);
-        }
-    }
-
-    /**
+     * /**
      * Metodo que muestra por consola una matrix bidirecional
      * 
      * @param matriz
@@ -26,7 +14,7 @@ public class Ejercicio6 {
      */
     public static void mostarMastrizOriginal() {
 
-        int[][] matrizOriginal = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        int[][] matrizOriginal = { { 1, 0, 0}, { 0, 5, 0 }, { 0, 0, 9 } };
         for (int i = 0; i < matrizOriginal.length; i++) {
             System.out.print(matrizOriginal[0][i]);
         }
@@ -38,7 +26,6 @@ public class Ejercicio6 {
         for (int i = 0; i < matrizOriginal.length; i++) {
             System.out.print(matrizOriginal[2][i]);
         }
-        System.out.println(" ");
         System.out.println(" ");
     }
 }
