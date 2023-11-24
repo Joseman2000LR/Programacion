577ade00c433cd6a8df0fa9883ed7687e87ed0c7
joseman2000 <jose12linares@gmail.com>
2023-11-24 16:24:38 +0000

    creacion de ejercicios

---
+++ Unidad_3/Ejercicios/EjercicioContinue_break/Ejercicio/src/main/java/ies/puerto/Ejercicio7.java
@@ -0,0 +1,10 @@
+package main.java.ies.puerto;
+
+/**
+ * Escribe un programa que imprima todos los números impares del 1 al 20, usando
+ * continue para omitir los números pares.
+ */
+
+public class Ejercicio7 {
+
+}
