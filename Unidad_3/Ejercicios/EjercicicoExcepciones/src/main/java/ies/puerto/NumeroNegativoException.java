7d936a94af70e01e8a70cd895dbb3842901ab5d7
joseman2000 <jose12linares@gmail.com>
2023-11-27 16:18:28 +0000

    creacion de ejercicio9 y la clase expecion

---
+++ Unidad_3/Ejercicios/EjercicicoExcepciones/src/main/java/ies/puerto/NumeroNegativoException.java
@@ -0,0 +1,9 @@
+package main.java.ies.puerto;
+
+public class NumeroNegativoException extends Exception {
+
+    public NumeroNegativoException(String mensaje) {
+        super(mensaje);
+    }
+
+}
