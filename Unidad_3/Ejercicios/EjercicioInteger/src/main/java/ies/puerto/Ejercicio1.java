7ed4f232101ff1b090146f080def5bc0b25f35a4
joseman2000 <jose12linares@gmail.com>
2023-12-01 18:40:50 +0000

    cambios y ejercicio de hechos

---
+++ Unidad_3/Ejercicios/EjercicioInteger/src/main/java/ies/puerto/Ejercicio1.java
@@ -0,0 +1,17 @@
+package ies.puerto;
+
+/**
+ * Crea un programa que tome un número entero como entrada y
+ * verifique si es par o impar utilizando los métodos de la clase
+ * Integer. Controla las exeption que pudiera generar.
+ */
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public void verificarParoImpar(int numero) {
+
+    }
+}
--- Unidad_3/Ejercicios/EjercicioString/src/main/java/ies/puerto/Ejercicio6.java
+++ Unidad_3/Ejercicios/EjercicioString/src/main/java/ies/puerto/Ejercicio6.java
@@ -1,11 +1,17 @@
+package ies.puerto;
 
 /***
- * Escribe un programa que tome una frase e imprima cada palabra en una nueva línea.
+ * Escribe un programa que tome una frase e imprima cada palabra en una nueva
+ * línea.
  */
 
 public class Ejercicio6 {
-    
+    public static void main(String[] args) {
+        String cadena = "Hola mundo";
+        char contador;
+        for (int i = 0; i < cadena.length(); i++) {
+            contador = cadena.charAt(i);
+            System.out.println(contador);
+        }
+    }
 }
-
-
-        
