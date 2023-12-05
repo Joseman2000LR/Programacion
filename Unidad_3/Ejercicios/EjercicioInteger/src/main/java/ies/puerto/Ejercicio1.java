c7569bc56a7d54bc333e540058622cb104f06943
José Manuel <71851066+Joseman2000LR@users.noreply.github.com>
2023-12-05 10:33:44 +0000

    ejercicio integer

--- Unidad_3/Ejercicios/EjercicioInteger/src/main/java/ies/puerto/Ejercicio1.java
+++ Unidad_3/Ejercicios/EjercicioInteger/src/main/java/ies/puerto/Ejercicio1.java
@@ -1,5 +1,8 @@
 package ies.puerto;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  * Crea un programa que tome un número entero como entrada y
  * verifique si es par o impar utilizando los métodos de la clase
@@ -7,11 +10,27 @@ package ies.puerto;
  */
 public class Ejercicio1 {
 
+    public static boolean verificarParoImpar(int numero) {
+        return Integer.remainderUnsigned(numero, 2) == 0;
+    }
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        try {
+            System.out.print("Ingrese un número entero: ");
+            int numero = scanner.nextInt();
+
+            if (verificarParoImpar(numero)) {
+                System.out.println(numero + " es un número par.");
+            } else {
+                System.out.println(numero + " es un número impar.");
+            }
+
+        } catch (InputMismatchException e) {
+            System.out.println("Error: Ingrese un número entero válido.");
+
+        }
 
     }
 
-    public void verificarParoImpar(int numero) {
-
-    }
 }
