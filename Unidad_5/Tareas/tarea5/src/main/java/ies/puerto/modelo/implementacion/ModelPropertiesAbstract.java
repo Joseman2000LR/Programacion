db77316c328b7f197282dc1860bbe266b0c3a465
José Manuel <71851066+Joseman2000LR@users.noreply.github.com>
2024-03-20 12:00:16 +0000

    cambios

--- Unidad_5/Tareas/tarea5/src/main/java/ies/puerto/modelo/implementacion/ModelPropertiesAbstract.java
+++
@@ -1,9 +0,0 @@
-package ies.puerto.modelo.implementacion;
-
-/**
- *
- * @author josem
- */
-public abstract class ModelPropertiesAbstract {
-    public static final String CSV_DELIMITADOR = ",";
-}
--- Unidad_5/Tareas/tarea5/src/main/java/ies/puerto/presentacion/Main.java
+++
@@ -1,9 +0,0 @@
-package ies.puerto.precentacion;
-
-public class Main {
-    public static void main(String[] args) {
-       
-
-
-    }
-}
